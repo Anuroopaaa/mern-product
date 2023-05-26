33b18b5da76584fd11da7a3096d3a566cd713ac7
Anuroopa K S <anuroopa.saji@gmail.com>
2023-05-26 15:49:41 +0530

    product last

--- backend/dB/index.js
+++ backend/dB/index.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://anuroopaks:Anuroopa123@cluster0.bde5pwt.mongodb.net/product')
- .then(() => {
-    console.log('Connected to MongoDB Atlas');
- })
- .catch(err => {
-    console.log("err"+err);
- })
+mongoose.connect('mongodb+srv://anuroopaks:Anuroopa@123@cluster0.bde5pwt.mongodb.net/product', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDb Atlas connection established')
+    })
+    .catch(err => {
+        console.log(err)
+    })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import Login from './Pages/Login.jsx';
 import AddItem from './Pages/AddItem';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 
 
 export default function App() {
@@ -13,15 +11,10 @@ export default function App() {
     <div>
       <Nav1></Nav1>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddItem/>} />
-          <Route path="/login" element={<Login />} />
-       </Routes>
-       <Container className="d-flex justify-content-center">
-       <h1 className='mx-5'>PRODUCTS</h1>
-      <Button variant="success">Add +</Button>{' '}
-
-       </Container>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddItem />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
